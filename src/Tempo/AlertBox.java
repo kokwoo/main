053ab86a953e6d80913762e67c576d1e8cf05dc2
Ashish <asj33@pitt.edu>
2015-10-17 07:26:18 -0400

    initial gui comit

---
+++ src/Tempo/AlertBox.java
@@ -0,0 +1,41 @@
+package Tempo;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+public class AlertBox {
+	static boolean value = false;
+	static String fname = "";
+	public static String display(String title,String message) {
+		
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		window.setMinWidth(250);
+		
+		Label label = new Label();
+		label.setText(message);
+		TextField filename = new TextField ();
+		filename.setPromptText("filename");
+		
+		Button closeButton = new Button("Enter");
+		closeButton.setOnAction(e -> {
+			fname = filename.getText().toString();
+			window.close();
+		});
+		
+		fname = filename.getText().toString();
+		System.out.println("fname 1" + fname);
+	
+		VBox layout = new VBox(10);
+		layout.setAlignment(Pos.CENTER);
+		layout.getChildren().add(filename);
+		layout.getChildren().add(closeButton);
+		Scene scene = new Scene(layout);
+		window.setScene(scene);
+		window.showAndWait();
+		return fname;
+	}
+
+}
